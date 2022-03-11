bbcbdc60233338c202535def83e6dbda49fc20cf
yuvrajs-ix <yuvrajs@incubxperts.com>
2022-03-11 18:34:04 +0530

    'changes'

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -5,5 +5,11 @@ login: "admin",
 password: "boomtown",
 options : {
 'sonar.sources': './src',
+// "sonar.exclusions": "**/tests/**",
+// "sonar.tests": "./tests",
+// "sonar.test.inclusions": "./tests/**/*",
+// 'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+// 'sonar.testExecutionReportPaths': 'test-report.xml',
+// 'sonar.eslint.reportPaths': 'eslint-report.json'
 }
 }, () => {});
