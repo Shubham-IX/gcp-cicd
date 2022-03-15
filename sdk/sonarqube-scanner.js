0a1d5440acaecedfcd199bbe7a0efdf71accbd03
Shubham-IX <shubhamh@incubxperts.com>
2022-03-15 17:20:49 +0530

    change

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -1,8 +1,8 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
-serverUrl: 'http://localhost:9000/',
+serverUrl: 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
 login: "admin",
-password: "boomtown",
+password: "Admin@123",
 options : { 
 'sonar.sources': './src',
 "sonar.exclusions": "**/tests/**",
