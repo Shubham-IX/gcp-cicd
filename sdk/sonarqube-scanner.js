2d198e53c0be7e0b9551ee385e3cfbf0d44969fd
Shubham-IX <shubhamh@incubxperts.com>
2022-03-15 17:18:42 +0530

    change

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -10,6 +10,10 @@ options : {
 "sonar.test.inclusions": "./tests/**/*",
 'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
 'sonar.testExecutionReportPaths': 'test-report.xml',
-'sonar.eslint.reportPaths': 'eslint-report.json'
+'sonar.eslint.reportPaths': 'eslint-report.json',
+'sonar.projectKey':'ix-pankajk_ix-terraform-cicd',
+'sonar.sources':'',
+'sonar.host.url': 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
+'sonar.login':'d5b085aefe7f549b999223032c62fcf4f3e56c50'
 }
 }, () => {});
