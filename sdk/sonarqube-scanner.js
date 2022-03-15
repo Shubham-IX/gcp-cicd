292c8aa35d5501b29af073a97f28f7d8fdd0ac7e
Shubham-IX <shubhamh@incubxperts.com>
2022-03-15 12:58:54 +0530

    changes

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -4,12 +4,13 @@ serverUrl: 'http://localhost:9000',
 login: "admin",
 password: "boomtown",
 options : {
+ 'sonar.host.url': 'http://localhost:9000',   
 'sonar.sources': './src',
-// "sonar.exclusions": "**/tests/**",
-// "sonar.tests": "./tests",
-// "sonar.test.inclusions": "./tests/**/*",
-// 'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
-// 'sonar.testExecutionReportPaths': 'test-report.xml',
-// 'sonar.eslint.reportPaths': 'eslint-report.json'
+"sonar.exclusions": "**/tests/**",
+"sonar.tests": "./tests",
+"sonar.test.inclusions": "./tests/**/*",
+'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+'sonar.testExecutionReportPaths': 'test-report.xml',
+'sonar.eslint.reportPaths': 'eslint-report.json'
 }
 }, () => {});
