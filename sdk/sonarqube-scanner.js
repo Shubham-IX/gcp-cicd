ad2147362a5cf991ec233db92edf29c83c2ab6c6
Shubham-IX <shubhamh@incubxperts.com>
2022-03-15 18:31:42 +0530

    change

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -1,6 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
-serverUrl: 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
+// serverUrl: 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
+serverUrl: 'http://localhost:9000',
 login: "admin",
 password: "Admin@123",
 options : { 
@@ -13,7 +14,8 @@ options : {
 'sonar.eslint.reportPaths': 'eslint-report.json',
 'sonar.projectKey':'ix-pankajk_ix-terraform-cicd',
 'sonar.sources':'',
-'sonar.host.url': 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
+//'sonar.host.url': 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
+'sonar.host.url': 'http://localhost:9000',
 'sonar.login':'d5b085aefe7f549b999223032c62fcf4f3e56c50'
 }
 }, () => {});
