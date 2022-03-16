c02cf922aa18b23509fcbd8795e7e8eb52fefe2b
Shubham-IX <shubhamh@incubxperts.com>
2022-03-16 10:17:27 +0530

    Merge branch 'development' of github.com:Shubham-IX/gcp-cicd into dev-shubham

--- sdk/sonarqube-scanner.js
+++ sdk/sonarqube-scanner.js
@@ -6,16 +6,5 @@ login: "admin",
 password: "Admin@123",
 options : { 
 'sonar.sources': './src',
-"sonar.exclusions": "**/tests/**",
-"sonar.tests": "./tests",
-"sonar.test.inclusions": "./tests/**/*",
-'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
-'sonar.testExecutionReportPaths': 'test-report.xml',
-'sonar.eslint.reportPaths': 'eslint-report.json',
-'sonar.projectKey':'ix-pankajk_ix-terraform-cicd',
-'sonar.sources':'',
-//'sonar.host.url': 'https://8080-cs-543085140216-default.cs-asia-southeast1-yelo.cloudshell.dev',
-'sonar.host.url': 'http://localhost:9000',
-'sonar.login':'d5b085aefe7f549b999223032c62fcf4f3e56c50'
 }
 }, () => {});
